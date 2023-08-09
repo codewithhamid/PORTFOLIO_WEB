8ed9d5f2579544cd7299be1c10c6f0199b85e4db
Md Hamid <95429239+codewithhamid@users.noreply.github.com>
2023-08-09 15:42:17 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ import { fileURLToPath} from "url";
 
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname=path,dirname(__filename);
+const __dirname=path.dirname(__filename);
 export const app = express();
 
 app.use(express.json({ limit: "50mb" }));
