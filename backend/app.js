6f020f4015c597e7dafa408732eacf00c68d5521
codewithhamid <95429239+codewithhamid@users.noreply.github.com>
2023-08-09 15:15:58 +0530

    final code
    mern

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import path, { dirname } from "path";
+import cors from 'cors';
+export const app = express();
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(cookieParser());
+app.use(cors());
+
+import { userRouter } from "./routes/User.js";
+app.use("/api/v1", userRouter);
+
+app.use(express.static(path.join(__dirname,"../frontend/build")));
+
+app.get("*",function(req,res){
+  res.sendFile(path.join(__dirname,"../frontend/build/index.html"))
+
+})
